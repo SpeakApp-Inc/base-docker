0a23a463150e9596a94cc38bcca4a8ec0b15ef08
Eduardo Cuomo <reduardo7@gmail.com>
2022-07-25 12:29:20 -0300

    Force upgrade base image

--- docker-upgrade.sh
+++ docker-upgrade.sh
@@ -2,10 +2,13 @@
 
 set -ex
 
+if docker images | grep -q node | grep -q 14-stretch-slim; then
+  docker rmi node:14-stretch-slim || true
+fi
+
 img=conversifi/base-node:node-14
 
 docker build -t ${img} .
 docker push ${img}
 
 echo Finish
-

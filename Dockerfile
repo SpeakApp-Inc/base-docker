4309a883e8bd1c2a3968e7afda4d329d22b7385b
Martin Ariel Castro <macastro90@hotmail.com>
2023-11-09 11:42:40 -0300

    New image for node-20

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM node:14-stretch-slim
+FROM node:20.9.0-slim
 
 RUN apt-get update && apt-get install -y curl git python make gcc g++
 
 RUN npm install -g npm
-RUN npm install -g lerna rimraf
+RUN npm install -g rimraf
 
 # > Node Sass could not find a binding for your current environment: OS X 64-bit with Node.js 14.x
 RUN npm rebuild node-sass
--- docker-upgrade.sh
+++ docker-upgrade.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-if docker images | grep -q node | grep -q 14-stretch-slim; then
-  docker rmi node:14-stretch-slim || true
+if docker images | grep -q node | grep -q 20.9.0-slim; then
+  docker rmi node:20.9.0-slim || true
 fi
 
-img=conversifi/base-node:latest
+img=conversifi/base-node:node-20
 
 docker build --platform linux/amd64 -t ${img} .
 docker push ${img}
